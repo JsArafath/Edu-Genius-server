3e140cf2aae2658885e776ecea140903f2a48c22
AnikHaque <ekramul15-811@diu.edu.bd>
2023-05-24 03:55:18 +0600

    folder , routes and others properly setted up

--- index.js
+++ index.js
@@ -1,40 +1,50 @@
-const express = require('express')
-const cors = require('cors');
-// const jwt = require('jsonwebtoken');
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
+const blogs = require("./routes/blogs");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app = express();
 
-// middleware 
+// middlewares
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
-// mongodb uri connection 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lx750.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+//use the doctor.js file to 
+//endpoints that start with /doctors
+
+app.use("/blogs", blogs);
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.k6jd9d0.mongodb.net/${process.env.DB}`;
+
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
-    try {
-      // database & collections 
-      const database = client.db("MarriageCenter");
-         const testCollection = database.collection("test");
-      console.log('Database Connection Successful');
+  try {
+    await client.connect();
+  
 
-      app.listen(port, () => {
-      console.log(`Example app listening on port ${port}`)
-})
-    } 
-    catch(err) {
-      console.log('There is an error Occured', err);
-    }
-    finally {   
-    }
+} catch (error) {
+    res.send(error);
   }
-  run()
-  .catch(console.dir);
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Running EduGenius Server");
+});
+
+app.get("/hero", (req, res) => {
+  res.send("Hero meets hero ku");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+})
