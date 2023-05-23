e6092e1bdc9383ef23ef17571a218baf974b6bdd
AnikHaque <ekramul15-811@diu.edu.bd>
2023-05-23 13:19:05 +0600

    index.js organized & file folder structured

--- index.js
+++ index.js
@@ -1,69 +1,40 @@
 const express = require('express')
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const jwt = require('jsonwebtoken');
 const cors = require('cors');
-const ObjectId = require('mongodb').ObjectId;
+// const jwt = require('jsonwebtoken');
+const { MongoClient } = require('mongodb');
 require('dotenv').config();
-const stripe = require("stripe")(process.env.STRIPE_SECRET);
 
 const app = express()
 const port = process.env.PORT || 5000;
-
-// middleware 
-app.use(cors());
-app.use(express.json()); 
-
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lx750.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const uri = "mongodb+srv://resellproduct:WtuwLvwyHhzfagBm@cluster0.w0dynz3.mongodb.net/?retryWrites=true&w=majority";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
-});
-
-
-function verifyToken(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: 'UnAuthorized access' });
-  }
-  
-  const token = authHeader.split(' ')[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
-    if (err) {
-      return res.status(403).send({ message: 'Forbidden access' })
-    }
-    req.decoded = decoded;
-    next();
-  });
-}
-async function run() { 
-    try {
-      // await client.connect();
-      const database = client.db("productsresellsite");
-     console.log('  Database Connection Successful');
-
-      app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-    } 
-    catch(err) {
-      console.log('Failed to connect Database' , err);
-    }
-    finally {
-      
-    }
-  }
-
-  run()
-  .catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+// middleware 
+app.use(cors());
+app.use(express.json()); 
 
+// mongodb uri connection 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lx750.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+      // database & collections 
+      const database = client.db("MarriageCenter");
+         const testCollection = database.collection("test");
+      console.log('Database Connection Successful');
+
+      app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`)
+})
+    } 
+    catch(err) {
+      console.log('There is an error Occured', err);
+    }
+    finally {   
+    }
+  }
+  run()
+  .catch(console.dir);
