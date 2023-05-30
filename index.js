7ad97aaa080dcf0e77c53b150bea4100f4b3d2dd
AnikHaque <ekramul15-811@diu.edu.bd>
2023-05-30 23:43:19 +0600

    added mongoose

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/mydatabase', {
